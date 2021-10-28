24095667fcde0d41c9cf0670880d911a320233d0
wangchunyang2 <wangchunyang2@yy.com>
2021-10-28 11:35:11 +0800

    feat: 压缩功能提交

--- main.js
+++ main.js
@@ -1,87 +1,13 @@
 import imagemin from 'imagemin'
-import imageminJpegtran from'imagemin-jpegtran'
 import imageminJpegRecompress from 'imagemin-jpeg-recompress'
 import imageminPngquant from 'imagemin-pngquant'
-import fs from 'fs'
+import {getCompressionRatio, getCompressImageJson, setCompressImageJson, mkdirsPromise, formatFileSize, consoleColor, findNotExistent} from './utils'
 import fsp from 'fs/promises'
 import path from 'path'
 
-const compressedImagesJsonPath = 'compressed.images.json'
+console.log(33)
 
-const defaultJson = {name: 'compressed images', version: '1.0.0', totalSourceFileSize: 0, totalCompressedFileSize: 0, compressionRatio: 0, compressedList: []}
-function getCompressImageJson() {
-  return fsp.readFile(compressedImagesJsonPath, 'utf-8').then(data => {
-    let value = defaultJson
-    try {
-      value = JSON.parse(data)
-    } catch (e) {}
-    return value
-  }).catch((e) => {
-    return defaultJson
-  })
-}
-
-function setCompressImageJson(data) {
-  return fsp.writeFile(compressedImagesJsonPath, JSON.stringify(data, null, 2), 'utf-8')
-}
-
-// 创建文件夹目录
-function mkdirsPromise(dirname) {
-  return new Promise((resolve) => {
-    const mkdirs = (dirname, callback) => {
-      fs.access(dirname, (err) => {
-        if (!err) {
-          callback()
-        } else {
-          mkdirs(path.dirname(dirname), () => {
-            fs.mkdir(dirname, callback)
-          })
-        }
-      })
-    }
-    mkdirs(dirname, resolve)
-  })
-}
-
-/**
- * 计算压缩比例，保留4位小数点
- * @param compressed
- * @param uncompressed
- * @return {number}
- */
-function getCompressionRatio(compressed, uncompressed) {
-  if (uncompressed === 0) {
-    return 0
-  }
-  return Math.floor((1 - compressed / uncompressed) * 1e4) / 1e4
-}
-
-function formatFileSize (value) {
-  const unitArray = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
-  const floatSize = parseFloat(value)
-  const index = Math.floor(Math.log(floatSize) / Math.log(1024))
-  const size = (floatSize / Math.pow(1024, index)).toFixed(2)
-  return size + unitArray[index]
-}
-
-function consoleColor(text) {
-  return `\x1b[31m${text}\x1b[0m`
-}
-
-// 过滤不存的文件
-function findNotExistent(compressedList) {
-  return new Promise((resolve, reject) => {
-    Promise.all(compressedList.map((item) => {
-      return fsp.access(item.sourcePath).then(() => null).catch((err) => {
-        return item.sourcePath
-      })
-    })).then((paths) => {
-      resolve(paths.filter((item) => item))
-    })
-  })
-}
-
-async function startCompress() {
+async function startImagemin() {
   const files = await imagemin(['src/**/*.{jpg,jpeg,png}'], {
     // destination: 'build/images',
     plugins: [
@@ -178,12 +104,4 @@ async function startCompress() {
   })
 }
 
-startCompress()
-
-// fsp.rename('./assets/ico_refrash@3x.png', './build/ico_refrash@3x.png').then(res => {
-//   console.log(res)
-// })
-
-// fsp.mkdir('./bak_compress/').then(res => {
-//   console.log(res)
-// })
+export default startImagemin
